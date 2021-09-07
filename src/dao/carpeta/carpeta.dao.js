97808aaec89dacb9245f60401c812b0d499ed1b7
LunaMan01 <luis-luna98@hotmail.com>
2021-09-07 14:25:58 -0500

    CRUD carpeta

--- src/dao/carpeta/carpeta.dao.js
+++ src/dao/carpeta/carpeta.dao.js
@@ -38,26 +38,34 @@ exports.getCarpeta = async (carpetaId) => {
 // Crear una carpeta
 exports.createCarpeta = async (carpetaData) => {
     try {
-        let carpetaCreated = await db.query("INSERT INTO Carpeta (nuc, tipo, presentacion, fecha_ingreso, fecha_inicio, duracion, judicalizada, nuevo, archivo_muerto, vinculacion, rol, oficio) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
-            carpetaData.nuc,
-            carpetaData.tipo,
-            carpetaData.presentacion,
+        let carpetaCreated = await db.query("INSERT INTO Carpeta (nuc_carpeta, tipo_carpeta, presentacion_carpeta, fecha_ingreso_carpeta, fecha_inicio_carpeta, duracion_carpeta, judicalizada_carpeta, nuevo_carpeta, archivo_muerto_carpeta, vinculacion_carpeta, rol_carpeta, oficio_carpeta) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [
+            carpetaData.nuc_carpeta,
+            carpetaData.tipo_carpeta,
+            carpetaData.presentacion_carpeta,
             new Date(),
             new Date(),
-            carpetaData.duracion,
-            carpetaData.judicalizada,
-            carpetaData.nuevo,
-            carpetaData.archivo_muerto,
-            carpetaData.vinculacion,
-            carpetaData.rol,
-            carpetaData.oficio
+            carpetaData.duracion_carpeta,
+            carpetaData.judicalizada_carpeta,
+            carpetaData.nuevo_carpeta,
+            carpetaData.archivo_muerto_carpeta,
+            carpetaData.vinculacion_carpeta,
+            carpetaData.rol_carpeta,
+            carpetaData.oficio_carpeta
         ]);
 
-        if (carpetaData.delitos.length != 0) {
+        if (carpetaData.delitos.length > 0) {
+            let newDelitos = carpetaData.delitos;
+            for (let i = 0; i < newDelitos.length; i++) {
+                if (!newDelitos[i].id_delito) {
+                    let delitoAdded = await db.query("INSERT INTO Delito (delito) VALUES (?)", newDelitos[i].delito);
+                    newDelitos[i].id_delito = delitoAdded.insertId;
+                }
+            }
+            carpetaData.delitos = newDelitos;
+
             let delitos = carpetaData.delitos;
-            let id = carpetaCreated.insertId;
             for (let i = 0; i < delitos.length; i++) {
-                let relation = await db.query("INSERT INTO CarpetaDelito (id_carpeta, id_delito) VALUES (?, ?)", [carpetaCreated.insertId, delitos[i]]);
+                let relation = await db.query("INSERT INTO CarpetaDelito (id_carpeta, id_delito) VALUES (?, ?)", [carpetaCreated.insertId, delitos[i].id_delito])
             }
         }
 
@@ -67,6 +75,47 @@ exports.createCarpeta = async (carpetaData) => {
                 inserted: carpetaCreated.insertId
             },
             code: 201
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Modificar carpeta
+exports.updateCarpeta = async (id_carpeta, dataToUpdate) => {
+    try {
+        let carpetaUpdated = await db.query(`UPDATE Carpeta SET nuc_carpeta = ?, tipo_carpeta = ?, presentacion_carpeta = ?, judicalizada_carpeta = ?, nuevo_carpeta = ?, archivo_muerto_carpeta = ?, vinculacion_carpeta = ?, rol_carpeta = ?, oficio_carpeta = ? WHERE id_carpeta = ?`, [
+            dataToUpdate.nuc_carpeta,
+            dataToUpdate.tipo_carpeta,
+            dataToUpdate.presentacion_carpeta,
+            dataToUpdate.judicalizada_carpeta,
+            dataToUpdate.nuevo_carpeta,
+            dataToUpdate.archivo_muerto_carpeta,
+            dataToUpdate.vinculacion_carpeta,
+            dataToUpdate.rol_carpeta,
+            dataToUpdate.oficio_carpeta,
+            id_carpeta]);
+
+        return {
+            message: "Carpeta actualizada!",
+            payload: { updated: id_carpeta },
+            code: 201
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Eliminar carpeta
+exports.deleteCarpeta = async (id_carpeta) => {
+    try {
+        let delitosDeleted = await db.query("DELETE FROM CarpetaDelito WHERE id_carpeta = ?", id_carpeta);
+        let carpetaDeleted = await db.query("DELETE FROM Carpeta WHERE id_carpeta = ?", id_carpeta);
+
+        return {
+            message: "Carpeta eliminada!",
+            payload: { id_deleted: id_carpeta },
+            code: 200
         }
     } catch (err) {
         console.log(err);
